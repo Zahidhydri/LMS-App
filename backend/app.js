c2d3f95b3b842e128df8d7bfcc5298f35f68a4dc
Zahid Sadique Hydri <zahidhydri20@gmail.com>
2025-07-21 16:17:30 +0530

    commit

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(morgan('dev'));
 
 app.use(cors({
-  origin: [process.env.CLIENT_URL, "http://localhost:3000"],
+  origin: [process.env.CLIENT_URL,"http://localhost:5173", "http://localhost:3000"],
   credentials: true
 }));
 
